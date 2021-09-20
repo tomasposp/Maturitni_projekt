b55d624241505adbaa0be09f07ea34e1f4dc23b9
tomasposp <77668409+tomasposp@users.noreply.github.com>
2021-09-20 16:44:55 +0200

    Lodě
    Rozeznání hittu a missu

---
+++ Maturitni_Projekt/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maturitni_Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            int[,] PoleLodi = new int[12, 12]; // velikost pole
+
+            Rectangle[,] PoleRect;
+            PoleRect = new Rectangle[10, 10];
+
+            VytvoreniPanelu(PoleRect);
+            xx
+        }
+        private void VytvoreniPanelu(Rectangle[,] pole)
+        {
+
+            for (int i = 0; i < pole.GetLength(1); i++)
+            {
+                mrizka.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                mrizka.RowDefinitions.Add(new RowDefinition());
+            }
+
+            int indexZed = 0;
+
+            for (int row = 0; row < pole.GetLength(0); row++)
+                for (int col = 0; col < pole.GetLength(1); col++)
+                {
+                    pole[row, col] = new Rectangle();
+
+                    pole[row, col].Fill = Brushes.Transparent;
+
+                    pole[row, col].StrokeThickness = 0;
+                    pole[row, col].Height = mrizka.Height / pole.GetLength(0);
+                    pole[row, col].Width = mrizka.Width / pole.GetLength(1);
+                    if (row == pole.GetLength(0) - 1 && col == pole.GetLength(1) - 1)
+                    {
+                        pole[row, col].Tag = -2;
+                    }
+                    if (row == 0 && col == 0) pole[row, col].Tag = 1;
+                    Grid.SetColumn(pole[row, col], col);
+                    Grid.SetRow(pole[row, col], row);
+                    Grid.SetZIndex(pole[row, col], 0);
+                    mrizka.Children.Add(pole[row, col]);
+                    indexZed++;
+
+                }
+        }
+
+
+    }
+}
