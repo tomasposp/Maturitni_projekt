7c34cd41626d8644d67c9f45c4d1dd61d7466b5b
tomasposp <77668409+tomasposp@users.noreply.github.com>
2021-10-25 19:16:50 +0200

    .

---
+++ Mat_projekt/Mat_projekt/Mat_projekt/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mat_projekt
+{
+    static class Class1
+    {
+        public static bool Easy;
+        public static bool Medium;
+
+
+    }
+}
---
+++ Mat_projekt/Mat_projekt/Mat_projekt/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mat_projekt
+{
+    /// <summary>
+    /// Interakční logika pro Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void SinglePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SingleVyber win = new SingleVyber();
+            win.Show();
+            this.Close();
+        }
+
+        private void MultiPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
