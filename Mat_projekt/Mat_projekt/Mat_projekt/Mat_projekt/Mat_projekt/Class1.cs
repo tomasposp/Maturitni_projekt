7bbf73aae81a459de39523f39490f24260ff6361
tomasposp <77668409+tomasposp@users.noreply.github.com>
2021-12-19 14:26:08 +0100

    Úprava singleplayeru

---
+++ Mat_projekt/Mat_projekt/Mat_projekt/Mat_projekt/Mat_projekt/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Mat_projekt
+{
+    static class Class1
+    {
+        public static bool Easy;
+        public static bool Medium;
+        public static Rectangle Rec2;
+
+    }
+}
---
+++ Mat_projekt/Mat_projekt/Mat_projekt/Mat_projekt/Mat_projekt/VyberObtiznostiPocitace.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mat_projekt
+{
+    /// <summary>
+    /// Interakční logika pro VyberObtiznostiPocitace.xaml
+    /// </summary>
+    public partial class VyberObtiznostiPocitace : Window
+    {
+        public VyberObtiznostiPocitace()
+        {
+            InitializeComponent();
+        }
+
+        private void Easy_Click(object sender, RoutedEventArgs e)
+        {
+            Class1.Easy = true;
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+            Class1.Medium = true;
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+    }
+}
